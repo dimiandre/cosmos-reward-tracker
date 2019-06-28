7153ffad0ab65de8ddfc62a718409d9851c7670a
dimiandre <dimiandre@gmail.com>
2019-06-28 17:37:02 +0200

    Add some widgets

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,38 @@
       <b-button type="submit" variant="primary">Track</b-button>
     </b-form>
 
+    <div class="mt-5" v-if="stats">
+        <h2>Stats</h2>
+
+
+        <b-card-group deck>
+            <b-card title="Delegations" v-if="shares > 0">
+                <b-card-text>
+                    You currently have <br />
+                    <big>{{ shares }} ATOMs delegated</big>
+                </b-card-text>
+                <div slot="footer"><small class="text-muted">Last updated 1 mins ago</small></div>
+            </b-card>
+
+            <b-card title="Estimated Reward" v-if="reward > 0">
+                <b-card-text>
+                    If you delegate all your ATOMs to stake.fish in one year you will get around <br /> 
+                    <big>{{ reward }} ATOMs</big>
+                </b-card-text>
+                <div slot="footer"><small class="text-muted">Last updated 1 mins ago</small></div>
+            </b-card>
+
+            <b-card title="Global Stats" v-if="bonded_atoms > 0">
+                <b-card-text>
+                    Total bonded ATOMs: {{ bonded_atoms }} <br />
+                    Total not bonded ATOMs: {{ not_bonded_atoms }}<br />
+                    Inflation: {{ inflation }}
+                </b-card-text>
+                <div slot="footer"><small class="text-muted">Last updated 1 mins ago</small></div>
+            </b-card>
+        </b-card-group>
+        
+    </div>
 
     <div class="mt-5" v-if="items.length > 0">
         <h2>Staking transactions</h2>
@@ -37,8 +69,14 @@ export default {
   data () {
     return {
         atomPrice: 0,
+        bonded_atoms: 0,
+        not_bonded_atoms : 0,
+        shares: 0,
+        inflation: 0,
+        reward: 0,
+        stats: false,
         form: {
-            address: 'cosmos1pdd93h0u3ats75relnqs99x2mpvkc5mvegn2tt' // cosmos1dw23avhehg6xtw9w2qkhcmy33nwv2wu3whezvh
+            address: 'cosmos1dw23avhehg6xtw9w2qkhcmy33nwv2wu3whezvh' // cosmos1pdd93h0u3ats75relnqs99x2mpvkc5mvegn2tt
         },
         msg: 'Welcome to Your Vue.js App',
         items: [
@@ -60,7 +98,7 @@ export default {
                 // TODO: consider transactions with more then one message
                 var tx = obj.tx.value.msg[0].value;
     
-                var atomAmount = (tx.amount.amount / 1000000).toFixed(2);
+                var atomAmount = self.convertMeasure(tx.amount.amount);
 
                 return {
                     height: obj.height,
@@ -72,7 +110,21 @@ export default {
                 }
             })
 
+            // Calculate statistics
+
+            // Get actual delegator delegations
+            self.getDelegations()
+
+            // Get atom stats
+            self.getAtomStats()
+
+            self.stats = true
+
         })
+      },
+
+      convertMeasure(atoms) {
+          return (atoms / 1000000).toFixed(2)
       },
 
       getAtomPrice() {
@@ -82,11 +134,49 @@ export default {
             this.$http.get(url).then(function(r) {
                 self.atomPrice = r.data.USD;
             })
+      },
+
+      getDelegations() {
+          var self = this
+
+            // call stargate api to get staking delegations
+            this.$http.get("https://stargate.cosmos.network/staking/delegators/" + this.form.address +"/delegations")
+            .then(function (res) {
+                
+                // Calculate total delegations
+                var shares = 0;
+                res.data.forEach(function(el) {
+                    shares += parseFloat(el.shares);
+                });
+
+                self.shares = self.convertMeasure(shares);
+                self.reward = (self.shares * 0.12).toFixed(2);
+            })
+      },
+
+      getAtomStats() {
+
+          // First get stake pool status
+          
+           var self = this
+
+            // call stargate api to get staking delegations
+            this.$http.get("https://stargate.cosmos.network/staking/pool")
+            .then(function (res) {
+                self.bonded_atoms = self.convertMeasure(res.data.bonded_tokens)
+                self.not_bonded_atoms = self.convertMeasure(res.data.not_bonded_tokens)
+            })
+
+            // Get inflation
+             this.$http.get("https://stargate.cosmos.network/minting/inflation")
+            .then(function (res) {
+                self.inflation = (res.data*100).toFixed(2) + " %"
+            })
       }
   },
 
   mounted() {
-      // Get actual Atom CmC Price
+      // Get actual Atom Price
       this.getAtomPrice()
   }
 
